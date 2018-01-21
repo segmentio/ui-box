060e573448379e910fca34ebec96a88bcc5b6880
Roland Warmerdam <roland@segment.com>
2018-01-20 22:49:12 -0800

    Tweak StyleSheet

--- src/css/style-sheet.js
+++ src/css/style-sheet.js
@@ -1,6 +1,5 @@
-// This file is loosely based on glamor
-import { getStyleTag } from './style-tag'
-
+// This file is based off glamor's StyleSheet
+// https://github.com/threepointone/glamor/blob/v2.20.40/src/sheet.js
 const isBrowser = typeof window !== 'undefined'
 const isDev = process.env.NODE_ENV === 'development' || !process.env.NODE_ENV
 const isTest = process.env.NODE_ENV === 'test'
@@ -22,12 +21,20 @@ function sheetForTag(tag) {
   }
 }
 
+function makeStyleTag() {
+  const tag = document.createElement('style')
+  tag.type = 'text/css'
+  tag.setAttribute('data-ui-box', '')
+  tag.appendChild(document.createTextNode(''))
+  ;(document.head || document.getElementsByTagName('head')[0]).appendChild(tag)
+  return tag
+}
+
 export default function StyleSheet({
   speedy = !isDev && !isTest,
   maxLength = 65000
 } = {}) {
-  // The big drawback here is that the css won't be editable in devtools
-  this.isSpeedy = speedy
+  this.isSpeedy = speedy // The big drawback here is that the css won't be editable in devtools
   this.sheet = undefined
   this.tags = []
   this.maxLength = maxLength
@@ -40,10 +47,10 @@ Object.assign(StyleSheet.prototype, {
   },
   inject() {
     if (this.injected) {
-      throw new Error('StyleSheet is already injected.')
+      throw new Error('StyleSheet has already been injected.')
     }
     if (isBrowser) {
-      this.tags[0] = getStyleTag()
+      this.tags[0] = makeStyleTag()
     } else {
       // Server side 'polyfill'. just enough behavior to be useful.
       this.sheet = {
@@ -74,7 +81,7 @@ Object.assign(StyleSheet.prototype, {
     } catch (err) {
       if (isDev) {
         // Might need beter dx for this
-        console.warn('StyleSheet illegal rule inserted', rule)
+        console.warn('StyleSheet inserted an illegal rule', rule)
       }
     }
   },
@@ -93,7 +100,7 @@ Object.assign(StyleSheet.prototype, {
 
     this.ctr += 1
     if (isBrowser && this.ctr % this.maxLength === 0) {
-      this.tags.push(getStyleTag())
+      this.tags.push(makeStyleTag())
     }
     return this.ctr - 1
   },
@@ -107,7 +114,6 @@ Object.assign(StyleSheet.prototype, {
       this.tags = []
       this.sheet = null
       this.ctr = 0
-      // Todo - look for remnants in document.styleSheets
     } else {
       // Simpler on server
       this.sheet.cssRules = []
