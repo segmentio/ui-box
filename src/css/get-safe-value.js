91e39711fb4a190a38f526c7a6f147978c485fb5
Roland Warmerdam <roland@segment.com>
2018-01-21 03:23:41 -0800

    Reuse regex

--- src/css/get-safe-value.js
+++ src/css/get-safe-value.js
@@ -1,9 +1,10 @@
 // https://stackoverflow.com/questions/7627000/javascript-convert-string-to-safe-class-name-for-css/7627603#7627603
+const unsafeCharactersRegex = /[!"#$%&'()*+,./:;<=>?@[\]^`{|}~]/g
+const spaceRegex = / /g
+
 export default function getSafeValue(value) {
   if (typeof value === 'string') {
-    return value
-      .replace(/[!"#$%&'()*+,./:;<=>?@[\]^`{|}~]/g, '')
-      .replace(/ /g, '-')
+    return value.replace(unsafeCharactersRegex, '').replace(spaceRegex, '-')
   }
 
   if (Number(value) === value) {
--- src/css/parse-unit.js
+++ src/css/parse-unit.js
@@ -1,8 +1,10 @@
+const unitRegex = /[\d.\-+]*\s*(.*)/
+
 export default function parseUnit(_str, _out = [0, '']) {
   const out = _out
   const str = String(_str)
   const num = parseFloat(str, 10)
   out[0] = num
-  out[1] = str.match(/[\d.\-+]*\s*(.*)/)[1] || ''
+  out[1] = str.match(unitRegex)[1] || ''
   return out
 }
