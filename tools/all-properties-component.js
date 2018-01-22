9cae03d2a9d95377d2edd91561f621060834482d
Roland Warmerdam <roland@segment.com>
2018-01-22 00:23:36 -0800

    Add a test for prop pass through

--- tools/all-properties-component.js
+++ tools/all-properties-component.js
@@ -5,6 +5,7 @@ import Box from '../src'
 export default function(includeGlamorProps = true) {
   return (
     <Box
+      contentEditable // Test prop pass through
       clearfix={includeGlamorProps ? true : undefined}
       backgroundColor="red"
       backgroundImage="url(https://s-media-cache-ak0.pinimg.com/736x/07/c3/45/07c345d0eca11d0bc97c894751ba1b46.jpg)"
