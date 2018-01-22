babee2ee980fddd6eb1a54ba8fbee0b1f8a23280
Roland Warmerdam <roland@segment.com>
2018-01-21 20:43:27 -0800

    Add unit tests for getClassName and getSafeValue

---
+++ test/css/get-class-name.js
@@ -0,0 +1,43 @@
+import test from 'ava'
+import getClassName from '../../src/css/get-class-name'
+
+test('supports inherit', t => {
+  t.is(getClassName({ key: 'width' }, 'inherit'), 'width_inherit')
+})
+
+test('supports initial', t => {
+  t.is(getClassName({ key: 'width' }, 'initial'), 'width_initial')
+})
+
+test('supports unset', t => {
+  t.is(getClassName({ key: 'width' }, 'unset'), 'width_unset')
+})
+
+test('supports numbers', t => {
+  t.is(getClassName({ key: 'width' }, 50.5), 'width_50-5px')
+})
+
+test('defaultUnit changes number unit', t => {
+  const result = getClassName({ key: 'width', defaultUnit: '' }, 50)
+  t.is(result, 'width_50')
+})
+
+test('lengthOnly only transforms dot and percentage', t => {
+  const result = getClassName({ key: 'width', lengthOnly: true }, '[50.5%')
+  t.is(result, 'width_[50-5prcnt')
+})
+
+test('numberOnly only transforms dot', t => {
+  const result = getClassName({ key: 'width', numberOnly: true }, '50.5%')
+  t.is(result, 'width_50-5%')
+})
+
+test('safeValue does not transform value', t => {
+  const result = getClassName({ key: 'width', safeValue: true }, '50.5%')
+  t.is(result, 'width_50.5%')
+})
+
+test('removes all unsafe values by default', t => {
+  const result = getClassName({ key: 'width' }, '50.5% calc(50.5% + 20px)')
+  t.is(result, 'width_50-5prcnt-calc50-5prcnt--20px')
+})
---
+++ test/css/get-safe-value.js
@@ -0,0 +1,14 @@
+import test from 'ava'
+import getSafeValue from '../../src/css/get-safe-value'
+
+test('replaces dots and spaces with dashes', t => {
+  t.is(getSafeValue('10.5px 20.5px 30.5px'), '10-5px-20-5px-30-5px')
+})
+
+test('replaces percentages with prcnt', t => {
+  t.is(getSafeValue('10% 20%'), '10prcnt-20prcnt')
+})
+
+test('strips unsafe characters', t => {
+  t.is(getSafeValue('calc(20px + 100%)'), 'calc20px--100prcnt')
+})
