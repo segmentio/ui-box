c2355572861820892fbae6cc66f2eac707322091
Roland Warmerdam <roland@segment.com>
2018-02-08 23:19:45 -0800

    Add more box tests

--- test/box.js
+++ test/box.js
@@ -1,6 +1,7 @@
 import test from 'ava'
 import React from 'react'
 import render from 'react-test-renderer'
+import { shallow } from 'enzyme'
 import Box from '../src/box'
 import * as styles from '../src/styles'
 import allPropertiesComponent from '../tools/all-properties-component'
@@ -39,4 +40,23 @@ test.serial('all properties set to initial', t => {
   const tree = render.create(component).toJSON()
   t.snapshot(tree, 'DOM')
   t.snapshot(styles.getAll(), 'CSS')
+})
+
+test('is prop changes the element type', t => {
+  const component = shallow(<Box is="h1" />)
+  t.true(component.is('h1'))
+})
+
+test('css prop renders a glamor class', t => {
+  const component = shallow(<Box css={{ height: '10px' }} />)
+  t.true(component.hasClass('css-882mhe'))
+})
+
+test('renders children', t => {
+  const component = shallow(
+    <Box>
+      <h1>Hi</h1>
+    </Box>
+  )
+  t.true(component.contains(<h1>Hi</h1>))
 })
