5359046909e73ef65a30e82f799e4f1e916e00df
Netto Farah <nettofarah@gmail.com>
2018-12-30 13:41:05 -0800

    Upgrade storybook

---
+++ .storybook/webpack.config.js
@@ -0,0 +1,15 @@
+module.exports = (_baseConfig, _env, config) => {
+  config.module.rules.push({
+    test: /\.(ts|tsx)$/,
+    use: [
+      {
+        loader: require.resolve('awesome-typescript-loader')
+      },
+      {
+        loader: require.resolve('react-docgen-typescript-loader')
+      }
+    ]
+  })
+  config.resolve.extensions.push('.ts', '.tsx')
+  return config
+}
