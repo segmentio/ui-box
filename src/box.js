eef320e1e5063b855d24ec77b9e537f002c38f23
Piper <piperchester@users.noreply.github.com>
2019-03-20 11:41:44 -0700

    fix: typo in deprecation warning (#32)

--- src/box.js
+++ src/box.js
@@ -37,7 +37,7 @@ export default class Box extends React.Component {
         if (!cssWarned) {
           cssWarned = true
           console.warn(
-            `📦 ui-box deprecation: the “css” prop will be removed in the next major version in favour of importing glamor directly and passing it՚s generated class to the “className” prop.`
+            `📦 ui-box deprecation: the “css” prop will be removed in the next major version in favour of importing glamor directly and passing its generated class to the “className” prop.`
           )
         }
       }
