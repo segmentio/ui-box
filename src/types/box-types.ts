7a18d1b6789e16eaf97059f5484c5a12f3ffa82a
Matt Shwery <mshwery@gmail.com>
2020-07-20 15:58:57 -0500

    update types again

--- src/types/box-types.ts
+++ src/types/box-types.ts
@@ -11,14 +11,16 @@ export type Without<T, K> = Pick<T, Exclude<keyof T, K>>
 
 export type PropsOf<
 	E extends keyof JSX.IntrinsicElements | React.JSXElementConstructor<any>
-> = JSX.LibraryManagedAttributes<E, React.ComponentPropsWithRef<E>>
+> = E extends React.JSXElementConstructor<infer P>
+  ? JSX.LibraryManagedAttributes<E, P>
+  : React.ComponentPropsWithRef<E>
 
 /**
  * Generic component props with "is" prop
  * @template P Additional props
  * @template T React component or string element
  */
-export type BoxOwnProps<E extends React.ElementType = React.ElementType> = EnhancerProps & {
+export type BoxOwnProps<E extends React.ElementType = React.ElementType, P = {}> = Without<EnhancerProps, keyof P> & {
   /**
    * Replaces the underlying element
    */
@@ -31,3 +33,20 @@ export type BoxOwnProps<E extends React.ElementType = React.ElementType> = Enhan
 }
 
 export type BoxProps<E extends React.ElementType> = BoxOwnProps<E> & Without<PropsOf<E>, keyof BoxOwnProps>
+
+/**
+ * Convenience type for defining your own component props that extend Box and pass-through props
+ */
+export type PolymorphicBoxProps<
+  E extends React.ElementType,
+  // optional additional props (which we get removed from BoxOwnProps and PropsOf)
+  // this is useful for defining some pass-through props on a wrapper for Box
+  P = {}
+> = BoxOwnProps<E, P> & Without<PropsOf<E>, keyof (BoxOwnProps & P)> & P
+
+/**
+ * Convenience type for defining your own components that extend Box and pass-through props
+ */
+export type BoxComponent<P = {}, D extends React.ElementType = React.ElementType> = <
+  E extends React.ElementType = D
+>(props: PolymorphicBoxProps<E, P>) => JSX.Element
