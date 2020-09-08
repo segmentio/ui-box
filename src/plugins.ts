cc63ccdfa7af1384a8f005252bbc749feeed29fe
Trey Griffith <trey.griffith@gmail.com>
2020-09-08 10:42:17 -0700

    add JSDoc to new plugin functions

--- src/plugins.ts
+++ src/plugins.ts
@@ -9,10 +9,18 @@ type Plugin = (set: RuleSet) => RuleSet
 
 const plugins: Plugin[] = []
 
+/**
+ * Adds a plugin that ui-box will apply before emitting styles into a stylesheet.
+ * Plugins are applied in the *opposite* order of insertion (most recent `use` first).
+ */
 export function use(plugin: Plugin): void {
   plugins.unshift(plugin)
 }
 
+/**
+ * Applies the added plugins to a given set of CSS rules.
+ * Should be used only internally by ui-box, not by plugin authors or consumers.
+ */
 export function apply(set: RuleSet) {
   let newSet = {...set}
 
@@ -23,6 +31,9 @@ export function apply(set: RuleSet) {
   return newSet
 }
 
+/**
+ * Removes all previously added plugins.
+ */
 export function clear(): void {
   plugins.length = 0
 }
