80240376b36e478b79fccd6c33d4c976d797ee14
Matt Shwery <matthew.shwery@segment.com>
2021-06-23 18:57:22 -0500

    fix memoized classnames

--- src/cache.ts
+++ src/cache.ts
@@ -1,39 +1,55 @@
-import {BoxPropValue} from './types/enhancers'
+import { BoxPropValue } from "./types/enhancers";
 
-type CacheValue = BoxPropValue
-let cache = new Map<string, string>()
+type CacheValue = BoxPropValue;
+let cache = new Map<string, string>();
 
-export function get(property: string, value: CacheValue) {
-  return cache.get(property + value)
+function getCacheKey(
+  property: string,
+  value: CacheValue,
+  type?: string
+): string {
+  // We need to differentiate between strings and numbers because
+  // the property may add units (like px) only for number types
+  const valueType = type || typeof value;
+  return property + (valueType === "string" ? `"${value}"` : value);
 }
 
-export function set(property: string, value: CacheValue | object, className: string) {
-  if (process.env.NODE_ENV !== 'production') {
-    const valueType = typeof value
+export function get(property: string, value: CacheValue) {
+  return cache.get(getCacheKey(property, value));
+}
+
+export function set(
+  property: string,
+  value: CacheValue | object,
+  className: string
+) {
+  const valueType = typeof value;
+
+  if (process.env.NODE_ENV !== "production") {
     if (
-      valueType !== 'boolean' &&
-      valueType !== 'number' &&
-      valueType !== 'string'
+      valueType !== "boolean" &&
+      valueType !== "number" &&
+      valueType !== "string"
     ) {
-      const encodedValue = JSON.stringify(value)
+      const encodedValue = JSON.stringify(value);
       throw new TypeError(
         `📦 ui-box: invalid cache value “${encodedValue}”. Only booleans, numbers and strings are supported.`
-      )
+      );
     }
   }
 
-  cache.set(property + value, className)
+  cache.set(getCacheKey(property, value as CacheValue, valueType), className);
 }
 
 export function entries() {
-  return [...cache]
+  return [...cache];
 }
 
-type CacheEntry = [/** key */ string, /** value */ string]
+type CacheEntry = [/** key */ string, /** value */ string];
 export function hydrate(newEntries: CacheEntry[]) {
-  cache = new Map<string, string>([...cache, ...newEntries])
+  cache = new Map<string, string>([...cache, ...newEntries]);
 }
 
 export function clear() {
-  cache.clear()
+  cache.clear();
 }
